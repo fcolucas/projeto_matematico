0cd2d8eaac3476ec0db37600f4cefc8f235d1d3a
Francisco Lucas <fcolucaslima14@gmail.com>
2018-01-22 11:10:50 -0300

    Modificações nas mensagens recebidas

--- pacResposta/main.c
+++ pacResposta/main.c
@@ -17,7 +17,7 @@ struct sockaddr_in remoto;
 
 struct pacote{
 	char head[4];
-	double numeros[10];
+	double numeros[20];
 };
 
 struct resposta{
@@ -25,61 +25,51 @@ struct resposta{
 	double resposta;
 };
 
-void gera_RESP(struct resposta *resp, int length, int id, int Op){
+void gera_RESP(struct resposta *resp, int length, int id, int Op, int total){
     resp->head[0] = 1; /*Tipo de pacote*/
     resp->head[1] = length; /*Tamanho do pacote*/
     resp->head[2] = id; /*Identificador da operação(o mesmo do pacote requisição)*/
     resp->head[3] = Op; /*Status da operaçao*/
+    resp->resposta = total;
 }
 
-int enviar_resposta(struct pacote *pac, struct resposta *resp, int client){
+void operacao(struct pacote *pac, struct resposta *resp){
     int Id = pac->head[2];
-    double total = 0;
-    int i=0, env;
+    double total = pac->numeros[0];
+    int i=0;
     switch(pac->head[3]){
         case 0: /*Operação de adição*/
-            for(i=0; i<pac->head[1]; i++){
+            for(i=1; i<pac->head[1]; i++){
                     total += pac->numeros[i];
                 }
-                gera_RESP(resp, pac->head[1], Id, 0);
-                resp->resposta = total;
-                env = send(client, resp, sizeof(struct resposta), 0);
+                gera_RESP(resp, pac->head[1], Id, 0, total);
                 break;
         case 1: /*Operação de subtração*/
-            for(i=0; i<pac->head[1]; i++){
+            for(i=1; i<pac->head[1]; i++){
                     total -= pac->numeros[i];
                 }
-                gera_RESP(resp, pac->head[1], Id, 0);
-                resp->resposta = total;
-                env = send(client, resp, sizeof(struct resposta), 0);
+                gera_RESP(resp, pac->head[1], Id, 0, total);
                 break;
         case 2: /*Operação de multiplicação*/
-            for(i=0; i<pac->head[1]; i++){
+            for(i=1; i<pac->head[1]; i++){
                     total *= pac->numeros[i];
                 }
-                gera_RESP(resp, pac->head[1], Id, 0);
-                resp->resposta = total;
-                env = send(client, resp, sizeof(struct resposta), 0);
+                gera_RESP(resp, pac->head[1], Id, 0, total);
                 break;
         case 3: /*Operação de divisão*/
-            for(i=0; i<pac->head[1]; i++){
+            for(i=1; i<pac->head[1]; i++){
                     total /= pac->numeros[i];
                 }
-                gera_RESP(resp, pac->head[1], Id, 0);
-                resp->resposta = total;
-                env = send(client, resp, sizeof(struct resposta), 0);
+                gera_RESP(resp, pac->head[1], Id, 0, total);
                 break;
     }
-    return env;
 }
 
 int main()
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     socklen_t len = sizeof(remoto);
-    int client, rec, env;
-    struct pacote *pac = (struct pacote*)malloc(sizeof(struct pacote));
-    struct resposta *resp = (struct resposta*)malloc(sizeof(struct resposta));
+    int client, rec, env, j;
 
     /*Conexão com o socket requisição*/
     if(sockfd == -1){
@@ -103,22 +93,35 @@ int main()
 		exit(1);
 	}
 
-	printf("Aguardando cliente...\n");
+	printf("Aguardando requisição...\n");
 
     client = accept(sockfd, (struct sockaddr *)&remoto, &len);
     if(client==-1){
         perror("accept ");
         exit(1);
     }
-    printf("Cliente conectado!...\n");
+    printf("Requisição conectado!\n");
 
     while(1){
+        struct pacote *pac = (struct pacote*)malloc(sizeof(struct pacote));
+        struct resposta *resp = (struct resposta*)malloc(sizeof(struct resposta));
+
         rec = recv(client, pac, sizeof(struct pacote), 0);
-        if(rec > 0){
+        printf("Recebido!\n");
+        if(rec == 168){ /*rec > 0*/
             printf("Recebido!\n");
-            printf("gerador de HEAD %d (requisicao) %d (tamanho) %d (ID) %d (OP)\n",pac->head[0],pac->head[1],pac->head[2],pac->head[3]);
-            env = enviar_resposta(pac, resp, client);
+            printf("Gerador de HEAD %d (requisicao) %d (tamanho) %d (ID) %d (OP)\n",pac->head[0],pac->head[1],pac->head[2],pac->head[3]);
+            for(j=0; j<pac->head[1]; j++){
+                printf("%lf ", pac->numeros[j]);
+            }
+            operacao(pac, resp);
+            env = send(client, resp, sizeof(struct resposta), 0);
+            if(env > 0) printf("Resposta; %lf\n\n", resp->resposta);
+            free(pac);
+            free(resp);
         }
     }
+    close(client);
+    close(sockfd);
     return 0;
 }
